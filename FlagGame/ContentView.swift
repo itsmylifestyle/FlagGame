f5b324981c5fa3d8b530057555910e99cff349a5
itsmylifestyle <hahaii00@mail.ru>
2022-11-02 15:33:54 +0600

    Initial Commit

---
+++ FlagGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlagGame
+//
+//  Created by Айбек on 02.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlagGame/FlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGameApp.swift
+//  FlagGame
+//
+//  Created by Айбек on 02.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
